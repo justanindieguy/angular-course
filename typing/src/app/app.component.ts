3b8f5b76ca285cb9d5beb38db796e22e74e90b84
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2021-01-09 15:38:54 -0600

    typing app finished.

---
+++ typing/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { lorem } from 'faker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  onInput(value: string): void {
+    this.enteredText = value;
+  }
+
+  compare(randomLetter: string, enteredLetter: string): string {
+    if (!enteredLetter) {
+      return 'has-text-gray';
+    }
+
+    return randomLetter === enteredLetter
+      ? 'has-text-success'
+      : 'has-text-danger has-text-weight-semibold';
+  }
+}
