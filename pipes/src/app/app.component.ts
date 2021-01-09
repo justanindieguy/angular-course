3b73fc35e3f248fb25efce123753ab0be045c8cf
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2021-01-09 15:38:07 -0600

    pipes exercise finished.

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+
+  onNameChange(value: string): void {
+    this.name = value;
+  }
+
+  onDateChange(value: string): void {
+    this.date = value;
+  }
+
+  private onNumberChange(value: string): number {
+    const parsedValue = parseFloat(value);
+
+    if (!isNaN(parsedValue)) {
+      return parsedValue;
+    }
+
+    return 0;
+  }
+
+  onAmountChange(value: string): void {
+    this.amount = this.onNumberChange(value);
+  }
+
+  onHeightChange(value: string): void {
+    this.height = this.onNumberChange(value);
+  }
+}
